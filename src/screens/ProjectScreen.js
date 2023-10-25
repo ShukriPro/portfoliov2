a09d314854cfcc3a79b29cfb834e312f858ee821
justs <justs@WINDOW12>
2023-10-25 22:40:19 +1300

    porject

--- src/screens/ProjectScreen.js
+++ src/screens/ProjectScreen.js
@@ -1,11 +1,51 @@
+import certificateImage1 from '../assets/Certificate/1.png';
+import certificateImage2 from '../assets/Certificate/2.png';
+import certificateImage3 from '../assets/Certificate/3.png';
+
+
 const ProjectScreen = () => {
-    return (
-        <div className="project-screen">
-      
+  const containerStyle = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    height: "100vh",
+    justifyContent: "center",
+  };
 
-        </div>
-    );
+  const imageStyle = {
+    width: "25rem",
+    height: "auto",
+    boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+    borderRadius: "10px",
+  };
+  return (
+    <div style={containerStyle}>
+      <h1>
+        PROJECT <span className="highlight">:</span>
+      </h1>
+      <h4>React.js / Java / C#</h4>
+      <div className="scrolling-container">
+        <img
+          style={imageStyle}
+          src={certificateImage1}
+          alt="Certificate 1"
+          className="scrolling-image"
+        />
+        <img
+          style={imageStyle}
+          src={certificateImage2}
+          alt="Certificate 2"
+          className="scrolling-image"
+        />
+        <img
+          style={imageStyle}
+          src={certificateImage3}
+          alt="Certificate 3"
+          className="scrolling-image"
+        />
+        {/* Add more images here */}
+      </div>
+    </div>
+  );
 };
-
-
 export default ProjectScreen;
